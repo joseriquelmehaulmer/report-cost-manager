9cc18d24fb7393b63f7888a844e87ee9c4cd1f56
Jose Riquelme <jriquelme@MacBook-Pro-de-Jose.local>
2023-11-21 14:04:25 -0300

    schedule change 15pm

--- src/functions/timerTrigger1.js
+++ src/functions/timerTrigger1.js
@@ -2,7 +2,7 @@ import { app } from '@azure/functions';
 import { main } from '../app.js';
 
 app.timer('timerTrigger1', {
-  schedule: '0 0 17 * * *',
+  schedule: '0 0 18 * * *',
   handler: async (myTimer, context) => {
     try {
       await main();
